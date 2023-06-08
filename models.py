df3d850f2b98732a39b68c0cb0853e57d515178d
rebeccanakhanu <rebeccanakhanu@gmail.com>
2023-06-08 17:08:52 +0300

    modified alembic,models.py envy.py

--- models.py
+++ models.py
@@ -2,6 +2,8 @@ from squalalchemy import cclumn,Integer,String,ForeignKey
 from squalaalchemy.orm import relationship
 from squalalchemy .ext.declarative import declarative_base
 
+engine = create_engine('sqlite:///roombooking.db',echo=True)
+
 Base = declarative_base()
 
 
