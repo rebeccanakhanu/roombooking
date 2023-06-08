577ef4cdf672740a922045494d72b307dd732bcc
rebeccanakhanu <rebeccanakhanu@gmail.com>
2023-06-08 16:49:24 +0300

    initializedd alembic

---
+++ models.py
@@ -0,0 +1,7 @@
+from squalalchemy import cclumn,Integer,String,ForeignKey
+from squalaalchemy.orm import relationship
+from squalalchemy .ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
