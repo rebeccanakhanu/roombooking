e36bd172d3627697293caa9d13ad0e195aa01e14
rebeccanakhanu <rebeccanakhanu@gmail.com>
2023-06-10 20:27:44 +0300

    modified files

--- models.py
+++ models.py
@@ -1,34 +1,32 @@
-from sqlalchemy import create_engine, Column,Integer,Date,String,ForeignKey,Table
-from sqlalchemy.orm import relationship,sessionmaker
+from sqlalchemy import (create_engine, Column,Integer,Date,String,ForeignKey,Table)
+from sqlalchemy.orm import relationship,sessionmaker,backref
 from sqlalchemy .ext.declarative import declarative_base
-
-
-engine = create_engine('sqlite:///roombooking.db',echo=True)
-Session = sessionmaker(bind=engine)
-session = Session()
-
 Base = declarative_base()
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///roombooking.db',echo=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
 # Define the association table for the many-to-many relationship between Room and Customer
-room_customer_association = Table(
-    'room_customer',
+room_customer = Table(
+   'room_customer',
     Base.metadata,
-    Column('room_id', Integer, ForeignKey('rooms.id')),
-    Column('customer_id', Integer, ForeignKey('customers.id'))
+    Column('room_id', ForeignKey('rooms.id')),
+    Column('customer_id', ForeignKey('customers.id')),
+        extend_existing=True,
+    
 )
 
 class Room(Base):
     __tablename__ = 'rooms'
     
     id= Column(Integer(),primary_key= True)
-    room_number = Column (String(250),nullable = False)
-    room_type = Column (String(250),nullable = False)
-    room_capacity = Column (Integer(),nullable = False)
-    room_price = Column (Integer(),nullable = False)
-    customer_id = Column(Integer, ForeignKey('customers.id'), unique=True)
-    
-    
-    customer = relationship("Customer", uselist=False, back_populates="room")
+    room_number = Column (Integer())
+    room_type = Column (String())
+    room_capacity = Column (Integer())
+    room_price = Column (Integer())
+    bookings=relationship("Booking",backref=backref("room"))
+    customers=relationship("Customer",secondary="room_customer",back_populates="rooms")
     
     def __repr__(self):
         return f'<Room id={self.id}, room_number={self.room_number}>'
@@ -49,24 +47,71 @@ class Room(Base):
             if room.is_available(check_in_date, check_out_date):
                 available_rooms.append(room)
         return available_rooms
+    
+    
+    def add_booking(self, customer, check_in_date, check_out_date):
+        if self.is_available(check_in_date, check_out_date):
+            booking = Booking(
+                room_id=self.id,
+                customer_id=customer.id,
+                check_in_date=check_in_date,
+                check_out_date=check_out_date
+            )
+            session.add(booking)
+            session.commit()
+            return booking
+        else:
+            return None
+
+    def remove_booking(self, booking):
+        session.delete(booking)
+        session.commit()
+
+    def get_total_price(self, check_in_date, check_out_date):
+        num_of_days = (check_out_date - check_in_date).days
+        return self.room_price * num_of_days
+
+    def get_occupancy(self, check_in_date, check_out_date):
+        bookings = self.get_bookings()
+        occupancy = 0
+        for booking in bookings:
+            if (
+                check_in_date < booking.check_out_date
+                and check_out_date > booking.check_in_date
+            ):
+                occupancy += booking.customer_count
+        return occupancy
+
+    def get_total_capacity(self):
+        return self.room_capacity
+
+    def get_available_capacity(self, check_in_date, check_out_date):
+        total_capacity = self.get_total_capacity()
+        occupancy = self.get_occupancy(check_in_date, check_out_date)
+        available_capacity = total_capacity - occupancy
+        return available_capacity
+
+    def is_full(self, check_in_date, check_out_date):
+        available_capacity = self.get_available_capacity(check_in_date, check_out_date)
+        return available_capacity == 0
 
     
 class Customer(Base):
     __tablename__ = 'customers'
     
     id= Column(Integer(),primary_key= True)
-    book_name= Column(String(250),nullable=False)
-    phone_number=(Column(Integer(),nullable=False))
-    email_address = Column(String(100),nullable=False)
-    
-    room = relationship("Room",uselist=False,back_populates="customer")
-    bookings = relationship("Booking", back_populates="customer")
+    book_name= Column(String())
+    phone_number = Column(Integer())
+    email = Column(String())
+    bookings = relationship("Booking",backref="customer")
+    rooms = relationship("Room", secondary="room_customer", back_populates="customers")
 
     def __repr__(self):
         return f'<Customer id={self.id}, name={self.book_name}>'
 
     def make_booking(self, room, check_in_date, check_out_date):
         if room.is_available(check_in_date, check_out_date):
+      
             booking = Booking(
                 room_id=room.id,
                 customer_id=self.id,
@@ -78,22 +123,69 @@ class Customer(Base):
             return booking
         else:
             return None
+        
+    def get_bookings(self):
+        return session.query(Booking).filter_by(customer_id=self.id).all()
+
+    def get_current_bookings(self):
+        today = Date.today()
+        return session.query(Booking).filter(
+            Booking.customer_id == self.id,
+            Booking.check_in_date <= today,
+            Booking.check_out_date >= today
+        ).all()
+
+    def get_past_bookings(self):
+        today = Date.today()
+        return session.query(Booking).filter(
+            Booking.customer_id == self.id,
+            Booking.check_out_date < today
+        ).all()
+
+    def get_upcoming_bookings(self):
+        today = Date.today()
+        return session.query(Booking).filter(
+            Booking.customer_id == self.id,
+            Booking.check_in_date > today
+        ).all()
+
 
 class Booking(Base):
    __tablename__ = 'bookings'
 
    id = Column(Integer(), primary_key=True)
-   customer_id = Column(Integer, ForeignKey('customers.id'))
+   customer_id = Column(Integer(), ForeignKey('customers.id'))
    check_in_date = Column(Date)
    check_out_date = Column(Date)
-    
-   customer = relationship("Customer", back_populates="bookings")
+   room_id= Column(Integer(), ForeignKey('rooms.id'))
    def __repr__(self):
         return f'<Booking id={self.id}, customer_id={self.customer_id}>'
+    
+    
+   def get_duration(self):
+        return (self.check_out_date - self.check_in_date).days
 
-def is_current(self):
+   def get_total_price(self):
+        room = session.query(Room).get(self.room_id)
+        return room.room_price * self.get_duration()
+
+   def is_current(self):
         today = Date.today()
         return self.check_in_date <= today <= self.check_out_date
+
+   def extend_booking(self, new_check_out_date):
+        if new_check_out_date > self.check_out_date:
+            self.check_out_date = new_check_out_date
+            session.commit()
+            return True
+        else:
+            return False
+
+   def cancel_booking(self):
+        room = session.query(Room).get(self.room_id)
+        room.remove_booking(self)
+        session.delete(self)
+        session.commit()
     
 
 
--- seed.py
+++ seed.py
@@ -1,45 +1,61 @@
 
 from faker import Faker
 from datetime import datetime, timedelta
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine,insert
 from sqlalchemy.orm import sessionmaker
 import random
 
-from models import Booking, Customer, Room
+from models import Booking, Customer, Room,room_customer
+if __name__ == "__main__":
+    fake = Faker()
+    engine = create_engine('sqlite:///roombooking.db')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.query(Booking).delete()
+    session.query(Customer).delete()
+    session.query(Room).delete()
+    session.query(room_customer).delete()
 
-fake = Faker()
-random.seed(42)  # Set a seed for consistent random data
-
-engine = create_engine('sqlite:///roombooking.db')
-Session = sessionmaker(bind=engine)
-session = Session()
-
-
-
-def seed_bookings():
-    rooms = session.query(Room).all()
-    customers = session.query(Customer).all()
-    bookings = []  # Empty list to hold added bookings
-
-    if not rooms:
-        print("No rooms found in the database.")
-        return bookings  # Return empty list
-
-    for _ in range(30):
-        room = random.choice(rooms)
-        customer = random.choice(customers)
-        check_in_date = fake.date_between(start_date='today', end_date='+30d')
-        check_out_date = check_in_date + timedelta(days=fake.random_int(min=1, max=7))
-
-        booking = booking(
-            room_id=room.id,
-            customer_id=customer.id,
-            check_in_date=check_in_date,
-            check_out_date=check_out_date
+    customers = []
+    for _ in range (20):
+        customer= Customer(
+            book_name=fake.name(),
+            phone_number=fake.phone_number(),
+            email=fake.email()
+        )
+        session.add(customer)
+        customers.append(customer)
+    rooms = []
+    for _ in range (20):
+        room = Room(
+           
+            room_number= random.randint(10,20),
+            
+            room_price=random.randint(100,200),
+            room_capacity=random.randint(10,20)
+            
+        )
+        session.add(room)
+        rooms.append(room)
+    existing_combinations=set()
+    for _ in range (20):
+        customer_id = random.randint(1,20)
+        room_id = random.randint(1,20)
+        if (customer_id,room_id)  in existing_combinations:
+            continue
+        existing_combinations.add((customer_id,room_id))
+        room_customer_data = {"customer_id":customer_id,"room_id":room_id}
+        stmt= insert(room_customer).values(room_customer_data)
+        session.execute(stmt)
+    
+    for _ in range(20):
+        booking = Booking(
+            customer_id=random.choice(customers).id,
+            check_in_date=datetime.now()+timedelta(days=random.randint(1,30)),
+            check_out_date=datetime.now()+timedelta(days=random.randint(1,30))
         )
         session.add(booking)
-        bookings.append(booking)  # Append the booking to the list
-
     session.commit()
-
-    return bookings  # Return the list of added bookings
+    session.close()
+        
+        
